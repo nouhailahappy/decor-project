6906f4c519baf766b1d68d2f5e78606917c3adf1
nouhailahappy <haappyhuman@gmail.com>
2020-06-13 00:53:53 +0100

    resp

--- script.js
+++ script.js
@@ -46,31 +46,3 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
-
- // sroll animation
-window.sr = ScrollReveal();
-sr.reveal(".animate-left", {
-  origin: "left",
-  duration: 1000,
-  distance: "25rem",
-  delay: 300
-});
-
-sr.reveal(".animate-right", {
-  origin: "right",
-  duration: 1000,
-  distance: "25rem",
-  delay: 600
-});
-sr.reveal(".animate-top", {
-  origin: "top",
-  duration: 1400,
-  distance: "25rem",
-  delay: 600
-});
-sr.reveal(".animate-bottom", {
-  origin: "bottom",
-  duration: 1000,
-  distance: "25rem",
-  delay: 600
-});
